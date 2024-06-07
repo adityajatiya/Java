c0ef8aa535717cfc76cebaba286f364c1b59cd9c
adityajatiya <adityajatiya23@gmail.com>
2024-06-07 13:14:29 +0530

    remove comments

--- src/javapractice/Basic/DataType/IntegerDataType.java
+++ src/javapractice/Basic/DataType/IntegerDataType.java
@@ -1,21 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javapractice.Basic.DataType;
 
 import java.util.Scanner;
 
-/**
- *
- * @author hp
- */
 public class IntegerDataType {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Your Age = ");
-        int  x = sc.nextInt();
+        int x = sc.nextInt();
         System.out.println(x);
 
         sc.close();

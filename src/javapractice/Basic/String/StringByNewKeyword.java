f8650fcfac0019f80632c2a0116a05e90cda4c9f
adityajatiya <169152050+adityajatiya@users.noreply.github.com>
2024-06-15 13:00:25 +0530

    Add files via upload

---
+++ src/javapractice/Basic/String/StringByNewKeyword.java
@@ -0,0 +1,17 @@
+package javapractice.Basic.DataType.String;
+
+import java.util.Scanner;
+
+public class StringByNewKeyword {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Name Enter = ");
+        String name = new String();
+        name = sc.nextLine();
+
+        System.out.print("Name = " + name + "\n");
+        name = "ram";
+        System.out.print("Name = " + name + "\n");
+    }
+}
